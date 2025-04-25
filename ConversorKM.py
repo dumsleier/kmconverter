1ec18882d7858ae311f2d83a9f7749204c32178d
Artur <pkllipsendereco@gmail.com>
2025-04-25 02:55:36 -0300

    Update ConversorKM.py
    Added try and except ValueError, removed unnecessary commentaries, made the code a bit cleaner. (Still simple)

--- ConversorKM.py
+++ ConversorKM.py
@@ -1,22 +1,19 @@
-# Just another quick newbie project
+print("Hello! What's your name and your desired distance (in km?)")
 
-# Welcome message
-print("Hello stranger! What's your name and your desired distance in km?")
-
-# User input to name and distance
 name = input('My name is: ')
 distancekm = input('My distance in km is: ')
 
-# Replace comma with period if needed
+# Replace comma(,) with period(.) if needed
 if ',' in distancekm:
     distancekm = distancekm.replace(',', '.')
 
-# Conversion of distance from km to miles
-distancekm = float(distancekm)
-distancemiles = distancekm / 1.609
+try:
+    distancekm = float(distancekm)
+    distancemiles = distancekm / 1.609
 
-# Output of the distance in miles
-print(f'How you doing, {name.title()}? Your distance of {distancekm}km in miles is: {distancemiles:.2f} mi')
+    print(f'Hi, {name.title()} :) Your distance of {distancekm}km in miles is: {distancemiles:.2f}mi')
 
-# Makes sure the user sees the result and press Enter to exit
-input('Press ENTER to exit.')
+except ValueError:
+    print("Please enter a valid number for the distance.")
+
+input('Press ENTER to exit. . . ')
